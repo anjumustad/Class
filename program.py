428079a3d7c4d04500534a12ffb95fc5d47404de
anjumustad <anjumustad0504@gmail.com>
2024-08-06 15:32:14 +0530

    mongoDB Connection

--- program.py
+++ program.py
@@ -15,22 +15,22 @@ collection1 = database["myCollection"] #creating new collection in demo1
 
 
 # #insertone 
-# dictionary = {"name": "Anjum","age": 24, "location": "Kolhapur"}
-# collection1.insert_one(dictionary)
+dictionary = {"name": "Anjum","age": 24, "location": "Kolhapur"}
+collection1.insert_one(dictionary)
 
 #now insertmanyfunction
 # To insertmanyfunction we need to create a list first.abs
 # after creating list give dictionary inside that list
-# insertList = [
-#     {"name": "Anjum", "age": 22, "location": "kop"},
-#     {"name": "Anjum", "age": 21, "location": "goa"},
-#     {"name": "vijay", "age": 20, "location": "delhi"},
-#     {"name": "pakkya", "age": 23, "location": "mumbai"},
-#     {"name": "shambhu", "age": 24, "location": "pune"}
-# ]
+insertList = [
+    {"name": "Anjum", "age": 22, "location": "kop"},
+    {"name": "Anjum", "age": 21, "location": "goa"},
+    {"name": "vijay", "age": 20, "location": "delhi"},
+    {"name": "pakkya", "age": 23, "location": "mumbai"},
+    {"name": "shambhu", "age": 24, "location": "pune"}
+]
 
 collection1.insert_many(insertList)
-# if we dont give id to out list then by deafult mongoDB will give
+# if we dont give id to our list then by deafult mongoDB will give
 #we can also give id to this by adding id in dict as "_id": 1 so on
 
 """
@@ -38,13 +38,13 @@ Now to read data
 
 """
 
-# read = collection1.find_one({"name": "pakkya"}) #find_one = find single doc
-# print(read)
+read = collection1.find_one({"name": "pakkya"}) #find_one = find single doc
+print(read)
 
 # to get all the doc at same time
-# allDocs = collection1.find({"name": "Anjum"}) #find = find all doc
-# for item in allDocs:
-#     print(item)
+allDocs = collection1.find({"name": "Anjum"}) #find = find all doc
+for item in allDocs:
+    print(item)
 
 
 #For Updating The Data
@@ -55,17 +55,17 @@ in update data there are 2 funtion : update_one & update_many
 #update One
 # we need to give or we need to give id from where we wanted to update the data
 
-# prev = {"name": "Anjum"}
-# next = {"$set": {"location": "Hyd"}}
+prev = {"name": "Anjum"}
+next = {"$set": {"location": "Hyd"}}
 
-# modified = collection1.update_one(prev,next)
+modified = collection1.update_one(prev,next)
 
 #update many or update all doc
 
-# prev = {"name": "Anjum"}
-# next = {"$set": {"location": "Hyd"}}
+prev = {"name": "Anjum"}
+next = {"$set": {"location": "Hyd"}}
 
-# modified = collection1.update_many(prev,next)
+modified = collection1.update_many(prev,next)
 
 
 #Delete data
@@ -73,8 +73,8 @@ in update data there are 2 funtion : update_one & update_many
 # delete many
 
 # #delete one
-# doc = {"name": "Anjum"}
-# collection1.delete_one(doc)
+doc = {"name": "Anjum"}
+collection1.delete_one(doc)
 
 #delete many
 
